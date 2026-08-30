ebce93fc13e59eece7a66d5004d47dc2059eb71a
gurjyan <gurjyan@jlab.org>
2026-08-29 21:22:14 -0400

    Pick first public IPv4 for NODE_IP, skip RFC-1918 and IPv6

--- env.sh
+++ env.sh
@@ -4,5 +4,6 @@
 export ERSAP_HOME=$(pwd)/ersap
 export ERSAP_USER_DATA=/global/cfs/cdirs/amsc016/haidis/ersap-data
 
-# Node IP -- first non-loopback address; used as recv-ip in services.yaml
-export NODE_IP=$(hostname -I | awk '{print $1}')
+# Node IP -- first public IPv4 address (skips RFC-1918 private ranges and
+# IPv6), used as recv-ip in services.yaml.
+export NODE_IP=$(hostname -I | tr ' ' '\n' | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$' | grep -v '^10\.' | grep -v '^172\.\(1[6-9]\|2[0-9]\|3[01]\)\.' | grep -v '^192\.168\.' | head -1)
