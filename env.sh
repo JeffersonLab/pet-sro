6798b885d6ce2f5ae7583ccfd0724a1620dee50d
gurjyan <gurjyan@jlab.org>
2026-08-29 21:16:48 -0400

    Set NODE_IP from hostname -I for use as ERSAP recv-ip

--- env.sh
+++ env.sh
@@ -3,3 +3,6 @@
 # ERSAP runtime paths (not defined in Dockerfile)
 export ERSAP_HOME=$(pwd)/ersap
 export ERSAP_USER_DATA=/global/cfs/cdirs/amsc016/haidis/ersap-data
+
+# Node IP -- first non-loopback address; used as recv-ip in services.yaml
+export NODE_IP=$(hostname -I | awk '{print $1}')
